16acadefe449e56a7dbe88e02138b16a888d8820
Jagruti Hota <jagrutihota92@gmail.com>
2024-12-28 22:53:38 +0530

    changed cookies to authorization to send token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,11 +3,16 @@ const jwt = require("jsonwebtoken");
 
 const userAuth = async (req, res, next) => {
   try {
-    const { token } = req.cookies; // Ensure cookie-parser middleware is used in the app
-    if (!token) {
-      return res.status(400).json({ message: "Please Login first" });
+    const authHeader = req.headers.authorization; // Retrieve the Authorization header
+
+    // Check if the Authorization header exists and starts with "Bearer "
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res
+        .status(400)
+        .json({ message: "Authorization token is missing or invalid" });
     }
 
+    const token = authHeader.split(" ")[1]; // Extract the token from the header
     const decodedObj = await jwt.verify(token, "DEV_STUDENT@123"); // Verify token
     const { _id } = decodedObj; // Extract the user id from token payload
 
